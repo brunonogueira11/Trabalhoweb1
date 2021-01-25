d153a6ea284547a201693d5d0aecec5895e652a8
brunonogueira11 <62335645+brunonogueira11@users.noreply.github.com>
2021-01-25 17:57:40 +0000

    Add files via upload

---
+++ isolpinhosite/scripts/main.js
@@ -0,0 +1,37 @@
+
+function myMap() {
+var mapOptions = {
+center: new google.maps.LatLng(40.2, -8.40),
+zoom: 10,
+mapTypeId: google.maps.MapTypeId.HYBRID
+};
+var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+}
+
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
